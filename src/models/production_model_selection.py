5330b17f5bfaed25e166dd2fc76e2fb52654988e
Matheus Sasso <Matheus_Sasso@gap.com>
2022-05-25 12:05:56 -0300

    feat:reforamt code

--- src/models/production_model_selection.py
+++ src/models/production_model_selection.py
@@ -5,6 +5,7 @@ from pprint import pprint
 from train_model import read_params
 from mlflow.tracking import MlflowClient
 
+
 def log_production_model(config_path):
     config = read_params(config_path)
     mlflow_config = config["mlflow_config"]
@@ -15,7 +16,9 @@ def log_production_model(config_path):
     mlflow.set_tracking_uri(remote_server_uri)
     runs = mlflow.search_runs(experiment_ids="1")
     max_accuracy = max(runs["metrics.accuracy"])
-    max_accuracy_run_id = list(runs[runs["metrics.accuracy"] == max_accuracy]["run_id"])[0]
+    max_accuracy_run_id = list(
+        runs[runs["metrics.accuracy"] == max_accuracy]["run_id"]
+    )[0]
 
     client = MlflowClient()
     for mv in client.search_model_versions(f"name='{model_name}'"):
@@ -26,22 +29,19 @@ def log_production_model(config_path):
             logged_model = mv["source"]
             pprint(mv, indent=4)
             client.transition_model_version_stage(
-                name=model_name,
-                version=current_version,
-                stage="Production"
+                name=model_name, version=current_version, stage="Production"
             )
         else:
             current_version = mv["version"]
             client.transition_model_version_stage(
-                name=model_name,
-                version=current_version,
-                stage="Staging"
+                name=model_name, version=current_version, stage="Staging"
             )
 
     loaded_model = mlflow.pyfunc.load_model(logged_model)
     joblib.dump(loaded_model, model_dir)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
